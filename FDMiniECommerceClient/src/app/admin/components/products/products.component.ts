f0e23bd270a3de6d6366f426e55aadccd6708ff4
FurkanDaloglu <furkan66129@gmail.com>
2024-09-07 20:35:35 +0300

    http-client-service created

--- FDMiniECommerceClient/src/app/admin/components/products/products.component.ts
+++ FDMiniECommerceClient/src/app/admin/components/products/products.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BaseComponent, SpinnerType } from '../../../base/base.component';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { HttpClientService } from '../../../services/common/http-client.service';
+declare var $: any;
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent extends BaseComponent {
-  constructor(spinner:NgxSpinnerService) {
-    super(spinner);
+export class ProductsComponent extends BaseComponent implements OnInit {
 
-    this.showSpinner(SpinnerType.BallAtom);
+  constructor(spinner:NgxSpinnerService,private httpClientService:HttpClientService) {
+    super(spinner);
   }
+    ngOnInit():void{
+      this.showSpinner(SpinnerType.BallAtom);
+      this.httpClientService.get({
+        controller:"products"
+      }).subscribe(data=>console.log(data));
+
+      // this.httpClientService.post({
+      //   controller:"products"
+      // },{
+      //   name:"Kalem",
+      //   stock:100,
+      //   price:15
+      // }).subscribe();
+
+      // this.httpClientService.put({
+      //   controller:"products",
+      // },{
+      //   id:"a477c978-2e7f-431f-88da-3901dac8eac9",
+      //   name:"A4 Kağıt",
+      //   stock:1500,
+      //   price:4.5
+      // }).subscribe();
+
+      // this.httpClientService.delete({
+      //   controller:"products"
+      // },"b7c7506b-deac-4d45-a46e-a1edbd78843c").subscribe();
+    }
 }
--- FDMiniEcommerceAPI/Presentation/FDMiniEcommerceAPI.API/Controllers/ProductsController.cs
+++ FDMiniEcommerceAPI/Presentation/FDMiniEcommerceAPI.API/Controllers/ProductsController.cs
@@ -1,6 +1,9 @@
 ﻿using FDMiniEcommerceAPI.Application.Repositories.ProductRepositories;
+using FDMiniEcommerceAPI.Application.ViewModels.Products;
+using FDMiniEcommerceAPI.Domain.Entites;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace FDMiniEcommerceAPI.API.Controllers
 {
@@ -19,16 +22,41 @@ namespace FDMiniEcommerceAPI.API.Controllers
 		[HttpGet]
 		public async Task<IActionResult> Get()
 		{
-			return Ok("Merhaba");
-			//await _productWriteRepository.AddRangeAsync(new()
-			//{
-			//	new() { Id=Guid.NewGuid(), Name="Product 1", Price =100, CreatedDate=DateTime.UtcNow,Stock=10 },
-			//	new() { Id=Guid.NewGuid(), Name="Product 2", Price =200, CreatedDate=DateTime.UtcNow,Stock=20 },
-			//	new() { Id=Guid.NewGuid(), Name="Product 3", Price =300, CreatedDate=DateTime.UtcNow,Stock=130 },
-			//});
-			//var count= await _productWriteRepository.SaveAsync();
-
-			
+			return Ok(_productReadRepository.GetAll(false));
+		}
+		[HttpGet("{id}")]
+		public async Task<IActionResult> Get(string id)
+		{
+			return Ok(await _productReadRepository.GetByIdAsync(id,false));
+		}
+		[HttpPost]
+		public async Task<IActionResult> Post(VM_Create_Product model)
+		{
+			await _productWriteRepository.AddAsync(new()
+			{
+				Name=model.Name,
+				Price=model.Price,
+				Stock=model.Stock
+			});
+			await _productWriteRepository.SaveAsync();
+			return StatusCode((int)HttpStatusCode.Created);
+		}
+		[HttpPut]
+		public async Task<IActionResult> Put(VM_Update_Product model)
+		{
+			Product product=await _productReadRepository.GetByIdAsync(model.Id);
+			product.Stock=model.Stock;
+			product.Name=model.Name;
+			product.Price=model.Price;
+			await _productWriteRepository.SaveAsync();
+			return Ok();
+		}
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> Delete(string id)
+		{
+			await _productWriteRepository.RemoveAsync(id);
+			await _productWriteRepository.SaveAsync();
+			return Ok();
 		}
 	}
 }
